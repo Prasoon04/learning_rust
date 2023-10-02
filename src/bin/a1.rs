ff88d1fc7397d0d1e68c7a3b5980396502ef6f3a
Prasoon Gautam <pgautam0411@gmail.com>
2023-10-02 16:02:10 -0500

    learning Rust

---
+++ src/bin/a1.rs
@@ -0,0 +1,15 @@
+// Print first and last name by calling functions.
+
+
+fn first_name() {
+    print!("Prasoon ");
+}
+
+fn last_name() {
+    println!("Gautam");
+}
+
+fn main() {
+    first_name();
+    last_name();
+}
